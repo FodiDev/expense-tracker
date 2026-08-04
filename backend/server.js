83b1a961849aeb43f19b4ba809d24f78279e9211
Lubabalo Fodi <fodi2592@gmail.com>
2026-08-04 16:03:45 +0200

    Updated server.js

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ import expenseRouter from './routes/expenseRoute.js';
 import dashboardRouter from './routes/dashboardRoute.js';
 
 const app = express();
-const port = process.env.PORT || 4000;
+const port = 4000;
 
 // Middleware
 app.use(cors());
