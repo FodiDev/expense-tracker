819483c4ac25fe8bfac91bbc28b8db8e212836a9
Lubabalo Fodi <fodi2592@gmail.com>
2026-08-04 15:51:03 +0200

    Server.js Update

--- backend/server.js
+++ backend/server.js
@@ -11,12 +11,7 @@ const app = express();
 const port = process.env.PORT || 4000;
 
 // Middleware
-app.use(
-  cors({
-    origin: ['http://localhost:5173', process.env.FRONTEND_URL],
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
